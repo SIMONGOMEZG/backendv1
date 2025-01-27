04db5d7db70a65909a789e2bf2221e15b047966e
SIMONGOMEZG <jsimongomezcardozo@gmail.com>
2025-01-27 07:20:01 -0500

    Update carts.router.js

--- tienda-tech/src/routes/carts.router.js
+++ tienda-tech/src/routes/carts.router.js
@@ -4,13 +4,11 @@ const CartManager = require('../managers/cartManager');
 const router = express.Router();
 const cartManager = new CartManager();
 
-// Ruta para crear un carrito
 router.post('/', async (req, res) => {
     const cart = await cartManager.createCart();
     res.status(201).json(cart);
 });
 
-// Ruta para agregar un producto a un carrito
 router.post('/:cid/products/:pid', async (req, res) => {
     const { cid, pid } = req.params;
     const updatedCart = await cartManager.addProductToCart(cid, pid);
